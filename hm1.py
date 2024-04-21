5e5ff1cf7104717f47207e4046ea8f843b6bc56b
Gamba <dmitriyi.nikitiuk@gmail.com>
2024-04-21 21:52:57 +0300

    v2 input removed from the func

--- hm1.py
+++ hm1.py
@@ -1,15 +1,13 @@
 from datetime import datetime
 import re
-def get_days_from_today():
-    while True:
-        date = input("Введіть дату у форматі 'РРРР-ММ-ДД': ")
-        date = re.sub(r'[^a-zA-Z0-9]', '-', date)
-        try:
-            date = datetime.strptime(date, "%Y-%m-%d")
-            today = datetime.today()
-            difference = today - date
-            return difference.days
-        except ValueError:
-            print("Неправильний формат дати. Спробуйте ще раз.")
+def get_days_from_today(date):
+    date = re.sub(r'[^a-zA-Z0-9]', '-', date)
+    try:
+        date = datetime.strptime(date, "%Y-%m-%d")
+        today = datetime.today()
+        difference = today - date
+        return difference.days
+    except ValueError:
+        print("Неправильний формат дати. Спробуйте ще раз.")
 
-print(get_days_from_today())
+print(get_days_from_today("2024-05-04"))
